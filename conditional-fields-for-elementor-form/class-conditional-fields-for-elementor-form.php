a7bbaedec074e6d6fae962a967d6377aaae10a3e
Sean Murphy <sean@iamseanmurphy.com>
2024-12-12 07:20:52 +0000

    1.3.6

--- conditional-fields-for-elementor-form/class-conditional-fields-for-elementor-form.php
+++ conditional-fields-for-elementor-form/class-conditional-fields-for-elementor-form.php
@@ -3,12 +3,12 @@
  * Plugin Name: Conditional Fields for Elementor Form
  * Plugin URI:https://coolplugins.net/
  * Description: The Conditional Fields for Elementor plugin add-on used to show and hide form fields based on conditional input values.
- * Version: 1.3.5
+ * Version: 1.3.6
  * Author:  Cool Plugins
  * Author URI: https://coolplugins.net/
  * License:GPL2
  * Text Domain:cfef
- * Elementor tested up to: 3.25.7
+ * Elementor tested up to: 3.25.10
  * Elementor Pro tested up to: 3.25.3
  *
  * @package cfef
@@ -20,7 +20,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit();
 }
 if ( ! defined( 'CFEF_VERSION' ) ) {
-	define( 'CFEF_VERSION', '1.3.5' );
+	define( 'CFEF_VERSION', '1.3.6' );
 }
 /*** Defined constent for later use */
 define( 'CFEF_FILE', __FILE__ );
@@ -97,7 +97,7 @@ if ( ! class_exists( 'Conditional_Fields_For_Elementor_Form' ) ) {
 		}
 
 		public function Cfef_plugin_redirection($plugin){
-			if ( ! is_plugin_active( 'elementor-pro/elementor-pro.php' ) ) {
+			if ( ! is_plugin_active( 'elementor-pro/elementor-pro.php' ) && ! is_plugin_active( 'pro-elements/pro-elements.php' ) ) {
 				return false;
 			}
 			if ( is_plugin_active( 'cool-formkit-for-elementor-forms/cool-formkit-for-elementor-forms.php' ) ) {
@@ -127,13 +127,19 @@ if ( ! class_exists( 'Conditional_Fields_For_Elementor_Form' ) ) {
 			new Conditional_Fields_Elementor_Page();
 		}
 		/**
-		 * Function use for deactivate plugin if elementor pro not exist
+		 * Function use for deactivate plugin if elementor pro or pro elements not exist
 		 */
 		public function is_elementor_pro_exist() {
-			if ( ! is_plugin_active( 'elementor-pro/elementor-pro.php' ) ) {
-					add_action( 'admin_notices', array( $this, 'admin_notice_missing_main_plugin' ) );
-					return false;
+			if (
+				is_plugin_active('pro-elements/pro-elements.php') || 
+				is_plugin_active('elementor-pro/elementor-pro.php')
+			) {
+				return true; // At least one plugin is active, the conditional plugin can run.
 			}
+		
+			// If neither plugin is active, show an admin notice.
+			add_action('admin_notices', array($this, 'admin_notice_missing_main_plugin'));
+			return false;
 		}
 
 		/**
@@ -143,12 +149,13 @@ if ( ! class_exists( 'Conditional_Fields_For_Elementor_Form' ) ) {
 			$message = sprintf(
 				// translators: %1$s replace with Conditional Fields for Elementor Form & %2$s replace with Elementor Pro.
 				esc_html__(
-					'%1$s requires %2$s to be installed and activated.',
+					'%1$s requires %2$s or %3$s to be installed and activated.',
 					'cfef'
 				),
 				esc_html__( 'Conditional Fields for Elementor Form', 'cfef' ),
-				esc_html__( 'Elementor Pro', 'cfef' )
-			);
+				esc_html__( 'Elementor Pro', 'cfef' ),
+				esc_html__( 'Pro Elements', 'cfef' )
+			); 
 			printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', esc_html( $message ) );
 			deactivate_plugins( plugin_basename( __FILE__ ) );
 		}
